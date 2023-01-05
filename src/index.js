84df33e94d34d6b35b5503eecc046a52e9f50361
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-05 17:59:07 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+    if (str[i] === '-') {
+        i += 1;
+    }
+    result = str[i] + result;
+    i += 1;
+  }
+  return Math.abs(result);
 }
